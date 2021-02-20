42285a0ea27bf7ee0e9261946d51d34a018c90b3
maniek <mpastuszka@gmail.com>
2021-02-20 10:28:05 +0100

    [init] - initial commit

---
+++ src/main/java/pl/sda/javalondek4springdemo/JavaLondek4SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.javalondek4springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLondek4SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLondek4SpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/javalondek4springdemo/JavaLondek4SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.javalondek4springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLondek4SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
